8f68234d550d07e130d6a85375a711f724d4e82b
tanmaydas31 <td3331@gmail.com>
2016-10-14 13:21:21 +0530

    Added Ques and Ans to its placeHolder
    Yaay !

---
+++ Assets/TriviaraLoop.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class TriviaraLoop : MonoBehaviour {
+
+    public struct Question {
+        public string questionText;
+        public string[] answers;
+        public int correctAnswerIndex;
+
+        public Question(string questionText, string[] answers, int correctAnswerIndex) {
+            this.questionText = questionText;
+            this.answers = answers;
+            this.correctAnswerIndex = correctAnswerIndex;
+        }
+    }
+
+    public Question currentQuestion = new Question("What is your favourite colour ?", new string[] { "Blue", "Red", "Yellow", "Green", "Black"}, 1);
+	// Use this for initialization
+	void Start () {
+		assignQuestion ();
+	}
+	public Text questionText;
+	public Button[] answerButton;
+
+    void assignQuestion() {
+		questionText.text = currentQuestion.questionText;
+		for (int i = 0; i < answerButton.Length; i++) {
+			answerButton [i].GetComponentInChildren<Text> ().text = currentQuestion.answers [i];
+		}
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
