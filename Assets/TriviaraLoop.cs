879f2bb49134a2a95802a0565cfc9bb729a3fb86
tanmaydas31 <td3331@gmail.com>
2016-10-14 13:38:42 +0530

    Buttons give feedback !
    correct displayed for correct button press, rearranged code, removed
    test from Main Camera ( not needed now.)

--- Assets/TriviaraLoop.cs
+++ Assets/TriviaraLoop.cs
@@ -17,23 +17,33 @@ public class TriviaraLoop : MonoBehaviour {
         }
     }
 
-    public Question currentQuestion = new Question("What is your favourite colour ?", new string[] { "Blue", "Red", "Yellow", "Green", "Black"}, 1);
+	public Text questionText;
+	public Button[] answerButton;
+
+	public Question currentQuestion = new Question("What is your favourite colour ?", new string[] { "Blue", "Red", "Yellow", "Green", "Black"}, 1);
+
+
 	// Use this for initialization
 	void Start () {
 		assignQuestion ();
 	}
-	public Text questionText;
-	public Button[] answerButton;
-
-    void assignQuestion() {
-		questionText.text = currentQuestion.questionText;
-		for (int i = 0; i < answerButton.Length; i++) {
-			answerButton [i].GetComponentInChildren<Text> ().text = currentQuestion.answers [i];
-		}
-    }
 
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void assignQuestion() {
+		questionText.text = currentQuestion.questionText;
+		for (int i = 0; i < answerButton.Length; i++) {
+			answerButton [i].GetComponentInChildren<Text> ().text = currentQuestion.answers [i];
+		}
+	}
+
+	public void checkAnswer(int answerButtonIndex){
+		if (answerButtonIndex == currentQuestion.correctAnswerIndex)
+			print ("CORRECT !");
+		else
+			print ("INCORRECT !");
 	}
 }
